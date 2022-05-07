c925547c34ebb336b8a4a42a6771414ab6714274
ikram3031 <mdikram.3031@gmail.com>
2022-05-07 18:23:57 +0600

    07-05-22

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-import { Article, Brand, CTA, Feature, Navbar} from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar/>
+            <Header/>
+        </div>   
+        <Brand/>
+        <WhatGPT3/>      
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+    </div>
   )
 }
 
